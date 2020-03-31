fed725b3a27d8728ae1d50fa5eaf065df600dac3
carolynhyu <hwiahyu@usc.edu>
2020-03-31 16:19:32 -0700

    Config addition
    Removed all html
    Implemented config file
    config lines are in are in all php files

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-	define('DB_HOST', '460.itpwebdev.com');
+	define('DB_HOST', 'localhost');
 	define('DB_USER', 'wasted_460delta');
 	define('DB_PASS', 'TeamDelta&2020');
 	define('DB_NAME', 'wasted_finalproject_db');
