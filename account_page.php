a562465a88bb9f2d19888d152b38d3bc6c3869f7
daizhuwu <daizhuwu@usc.edu>
2020-04-14 12:05:25 -0700

    Add files via upload
    Updated account page and added edit confirmation page

--- account_page.php
+++ account_page.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
 
   require "config/config.php";
 
@@ -11,17 +11,22 @@
 
   $mysqli->set_charset('utf8');
 
-
+  //save form input into session variables
   $email = $_SESSION['user_email'];
   $password = $_SESSION['user_password'];
 
+  //retrieve the row of info which is user selected
   $sql_user = "SELECT * FROM users WHERE user_email='$email' AND user_password='$password';";
-        $results_user = $mysqli->query($sql_user);
-        if ( !$results_user ) {
-            echo $mysqli->error;
-            $mysqli->close();
-            exit();
-        }
+  $results_user = $mysqli->query($sql_user);
+
+  //print_r($results_user);
+  
+  if ( $results_user->num_rows <=0 ) {
+      echo "Can't find user, please login first";
+      echo $mysqli->error;
+      $mysqli->close();
+      exit();
+  }
 
   $row = $results_user->fetch_assoc();
 
@@ -34,6 +39,17 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- Hotjar Tracking Code for http://460.itpwebdev.com/~bo/-daizhuwu -->
+<script>
+  (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:1752462,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
     <link rel="stylesheet" href="assets/css/bootstrap_nav.css" />
     <link
       rel="stylesheet"
@@ -235,10 +251,183 @@
             .clearfix button {
                 width: 100px;
             }
+
+            /* Account Page */
+            h5 {
+              /* display: block; */
+              margin-top: 40px;
+              margin-bottom: 40px;
+            }
+            #navbar {
+              position: fixed;
+              top: 0;
+              bottom: 0;
+              left: 0;
+
+              background-color:#a0d468;
+            }
         </style>
   </head>
   <body>
-      <?php include ('navbar.php'); ?>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-2 sidebar-outer">
+          <div
+            id="sidebar-container"
+            class="sidebar-expanded d-none d-md-block"
+          >
+            <!-- Bootstrap List Group -->
+            <ul id="navbar" class="list-group">
+              <!-- SECTION 1 -->
+              <div id="nav-header">
+                <li class="nav-item toggle">
+                  <h3 class="nav-link" href="#">
+                    <img id="nav-logo" src="assets/img/logo.png" alt="Wasted logo" />
+                    <span id="wasted" class="desc">Wasted</span>
+                  </h3>
+                  <p>Hey Sara!</p>
+
+                  <!-- <a id="arrow-a" href="#">
+                    <div id="arrow-container">
+                      <img id="arrow" src="img/arrow.png" alt="arrow">
+                    </div>
+                  </a> -->
+                </li>
+
+                <!-- <div>
+                    <p class="toggle">Hey Sara!</p>
+                </div> -->
+              </div>
+              <!-- nav header -->
+
+             <a
+                href="#top"
+                data-toggle="sidebar-collapse"
+                class="list-group-item list-group-item-action d-flex align-items-center"
+                style="background-color: #a0d468;"
+              >
+                <div
+                  class="d-flex w-100 justify-content-start align-items-center"
+                >
+                  <!-- <h3 class="nav-link toggle" href="#">
+                                <img id="nav-logo" src="img/logo.png" alt="Wasted logo">
+                                <span id="wasted" class="desc">Wasted</span>
+                            </h3> -->
+                  <span
+                    id="collapse-icon"
+                    class="fa fa-angle-double-left fa-2x mr-3"
+                  ></span>
+                  <span id="collapse-text" class="menu-collapsed"
+                    >Collapse</span
+                  >
+                </div>
+              </a>
+
+              <!-- SECTION 2 -->
+              <a
+                aria-expanded="false"
+                class="list-group-item list-group-item-action flex-column align-items-start"
+                id="dashboard"
+                style="background-color: #a0d468;"
+                href="dashboard.html"
+              >
+                <div
+                  class="d-flex w-100 justify-content-start align-items-center"
+                >
+                  <span class="fa fa-home fa-fw mr-3"></span>
+                  <span class="menu-collapsed">Dashboard</span>
+                </div>
+              </a>
+
+              <a
+                aria-expanded="false"
+                class="list-group-item list-group-item-action flex-column align-items-start"
+                style="background-color: #a0d468;"
+                href="fridge-list.html"
+              >
+                <div
+                  class="d-flex w-100 justify-content-start align-items-center"
+                >
+                  <span class="fa fa-igloo fa-fw mr-3"></span>
+                  <span class="menu-collapsed">Fridge</span>
+                </div>
+              </a>
+
+              <a
+                aria-expanded="false"
+                class="list-group-item list-group-item-action flex-column align-items-start"
+                style="background-color: #a0d468;"
+                href="#"
+              >
+                <div
+                  class="d-flex w-100 justify-content-start align-items-center"
+                >
+                  <span class="fa fa-utensils fa-fw mr-3"></span>
+                  <span class="menu-collapsed">Recipes</span>
+                </div>
+              </a>
+
+              <!-- <a
+                aria-expanded="false"
+                class="list-group-item list-group-item-action flex-column align-items-start"
+                style="background-color: #a0d468;"
+                href="#"
+              >
+                <div
+                  class="d-flex w-100 justify-content-start align-items-center"
+                >
+                  <span class="fa fa-dashboard fa-fw mr-3"></span>
+                  <span class="menu-collapsed">Reports</span>
+                </div>
+              </a> -->
+
+              <!-- SECTION 3 -->
+              <a
+                aria-expanded="false"
+                class="list-group-item list-group-item-action flex-column align-items-start"
+                id="my-acct"
+                style="background-color: #a0d468;"
+                href="account_page.html"
+              >
+                <div
+                  class="d-flex w-100 justify-content-start align-items-center"
+                >
+                  <span class="fa fa-user fa-fw mr-3"></span>
+                  <span class="menu-collapsed">My Account</span>
+                </div>
+              </a>
+
+              <a
+                aria-expanded="false"
+                class="list-group-item list-group-item-action flex-column align-items-start"
+                style="background-color: #a0d468;"
+                href="logout.php"
+              >
+                <div
+                  class="d-flex w-100 justify-content-start align-items-center"
+                >
+                  <span class="fas fa-sign-out-alt fa-fw mr-3"></span>
+                  <span class="menu-collapsed">Log Out</span>
+                </div>
+              </a>
+              <!-- ????? -->
+
+              <!-- add ingredients -->
+              <div id="nav-add">
+                <li class="nav-item" id="nav-add-li">
+                  <a id="nav-add-a" class="nav-link" href="add-ingredients.html">
+                    <img src="assets/img/plus.png" alt="Add ingredients" />
+                    <span class="desc toggle">Add Ingredient</span>
+                  </a>
+                </li>
+              </div>
+              <!-- nav-add  -->
+            </ul>
+            <!-- List Group END-->
+          </div>
+          <!-- sidebar-container  END -->
+          <!-- MAIN -->
+        </div>
 
         <!-- MAIN CONTENT FOR EACH PAGE -->
 
@@ -253,26 +442,42 @@
                       <!-- <form class="modal-content" action="/action_page.php"> -->
                         <div class="container">
                           <!-- <p>Please fill in this form to create an account.</p> -->
+                
                           <hr>
+
+                        <form action="edit_confirmation.php" method="POST">  
+                        <input type="hidden" name="user_id" value="<?php echo $row['user_id']; ?>">                    
+
+                          <h5>Account Information</h5>
+
+                          <!-- Pre-populate -->
                           <label for="name"><b>First Name</b></label>
-                          <input type="text" placeholder="<?php echo $row['user_firstname'];?>" name="name" required>
+                          <input type="text" placeholder="First Name" name="name" 
+                          value="<?php echo $row['user_firstname']; ?>" required>
 
                           <label for="name"><b>Last Name</b></label>
-                          <input type="text" placeholder="<?php echo $row['user_lastname'];?>" name="name" required>
+                          <input type="text" placeholder="Last Name" name="name" 
+                          value="<?php echo $row['user_lastname']; ?>" required>
 
                           <label for="email"><b>Email</b></label>
-                          <input type="text" placeholder="<?php echo $row['user_email'];?>" name="email" required>
-                    
-                          <label for="psw"><b>Password</b></label>
-                          <input type="password" placeholder="Password" name="psw" required>
+                          <input type="text" placeholder="Email" name="email" 
+                          value="<?php echo $row['user_email']; ?>" required>
 
-                    
+                          <h5>Account Password</h5>
+
+                          <!-- Don't pre-populate -->
+                          <label for="psw"><b>Current Password<span class="text-danger">*</span></b></label>
+                          <input type="password" placeholder="Password" name="current_psw" required>
+
+                          <label for="psw"><b>New Password<span class="text-danger">*</span></b></label>
+                          <input type="password" placeholder="Password" name="new_psw" required>
                     
                           <div class="clearfix">
-                            <button>Edit</button>
-                            <button>Save</button>
+                            <button type="submit">Save</button>
+                            <button type="reset">Reset</button>
                           </div>
                         </div>
+                      </form>
                       
               </div> <!--END of MAIN-CONTENT-->
 
